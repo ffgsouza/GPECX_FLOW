aec9b0078fc7b75714a5f51b00b691eca28587ea
Francisco Fabio <ffgsouza25@gmail.com>
2025-08-27 02:04:14 +0000

    segue valores default , Frete = $ 575, UTS 500 = $ 8300, hardware = 30%

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import type { Product, Settings } from './types';
 
 export const INITIAL_PRODUCTS: Product[] = [
-  { id: '2', name: 'UTS 500', hardwareCostUSD: 8300, softwareCostUSD: 0, freightCostUSD: 91 },
+  { id: '2', name: 'UTS 500', hardwareCostUSD: 2490, softwareCostUSD: 5810, freightCostUSD: 575 },
 ];
 
 export const INITIAL_SETTINGS: Settings = {
