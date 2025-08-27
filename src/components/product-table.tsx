b246670bc2d247cf439a7bf0e7cef85d47b5850f
Francisco Fabio <ffgsouza25@gmail.com>
2025-08-27 01:45:27 +0000

    corrija o erro

--- src/components/product-table.tsx
+++ src/components/product-table.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -313,7 +313,7 @@ export function ProductTable() {
                               <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
                               <AlertDialogDescription>
                                 Esta ação não pode ser desfeita. Isso excluirá permanentemente o produto "{product.name}".
-                              </DialogDescription>
+                              </AlertDialogDescription>
                             </AlertDialogHeader>
                             <AlertDialogFooter>
                               <AlertDialogCancel>Cancelar</AlertDialogCancel>
